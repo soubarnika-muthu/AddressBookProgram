e62d8d61efbf4b85c10eb2487411d42eedb35459
soubarnika-muthu <venkatachalapathymuthu@gmail.com>
2021-07-03 19:43:18 +0530

    [soubarnika]UC1-Adding New Contact in Address Book

--- AddressBook/Program.cs
+++ AddressBook/Program.cs
@@ -17,23 +17,53 @@ namespace AddressBook
             public string Country { get; set; }
             public int PhoneNo { get; set; }
             public string Email { get; set; }
-         }
+        }
         //Creating  list to store the person in Address Book
         public static List<Person> list = new List<Person>();
 
 
+        static string userStringEntry;
+        static int userChoice;
 
-        public static void Main(string[] args)
+
+
+        static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Address Book Program!");
-          
-            CreateContacts();//Calling createcontacts method  to create new contacts in Address Book
+            MainMenu();
+
+            Console.ReadKey();
         }
-            //A function to get contact details from the user
-            static void CreateContacts()
+        static void MainMenu()
+        {
+
+
+            Console.WriteLine("Choose the option ");
+            Console.WriteLine("\n1 - Add Contact.");
+
+
+            userStringEntry = Console.ReadLine();
+            userChoice = System.Int32.Parse(userStringEntry);
+            //selection statement
+            switch (userChoice)
             {
-               Person person = new Person();//Creating object for class person
-            Console.WriteLine("--Creating New contact Details :--");
+                case 1:
+                    Console.WriteLine("Adding  New Contacts");
+                    AddContact();
+                    
+                    break;
+                default:
+                    break;
+            }
+        }
+        //A function to ADD contact details of the user
+        static void AddContact()
+        {
+            do
+            {
+
+                Person person = new Person();//Creating object for class person
+
                 //Getting Contact Details  from the User
                 Console.WriteLine("Please Enter your First Name:");
                 person.FirstName = Convert.ToString(Console.ReadLine());
@@ -55,29 +85,44 @@ namespace AddressBook
 
                 Console.WriteLine("Please Enter your EmailAddress");
                 person.Email = Convert.ToString(Console.ReadLine());
+                //Adding person details to the list using ADD();
+                list.Add(person);
+                //Displaying the Contact Details of the person     
+                Console.WriteLine("***********************************");
+                Console.WriteLine("    Address Book  of the person     ");
+                Console.WriteLine("***********************************");
+                Console.WriteLine("First Name:" + person.FirstName);
+                Console.WriteLine("Last Name:" + person.LastName);
+                Console.WriteLine("Address:" + person.Address);
+                Console.WriteLine("State:" + person.State);
+                Console.WriteLine("Country:" + person.Country);
+                Console.WriteLine("Phone Number:" + person.PhoneNo);
+                Console.WriteLine("Mail :" + person.Email);
+                Console.WriteLine("***********************************");
 
-                
-        Console.WriteLine("***********************************");
-        Console.WriteLine("    Address Book  of the person     ");
-        Console.WriteLine("***********************************");
-        Console.WriteLine("First Name:" + person.FirstName);
-        Console.WriteLine("Last Name:" + person.LastName);
-        Console.WriteLine("Address:" + person.Address);
-        Console.WriteLine("State:" + person.State);
-        Console.WriteLine("Country:" + person.Country);
-        Console.WriteLine("Phone Number:" + person.PhoneNo);
-        Console.WriteLine("Mail :" + person.Email);
-        Console.WriteLine("***********************************");
+                Console.WriteLine("\nIs the following information correct?");
+                Console.WriteLine("\n1 - Yes. 2 - Retry.");
+
+                userStringEntry = Console.ReadLine();
+                userChoice = System.Int32.Parse(userStringEntry);
+
+            }
+            while (userChoice == 2);
+
+            if (userChoice == 1)
+            {
+                MainMenu();
+            }
+        }
 
     }
-
-
-
-
-
-
 }
-}
+
+
+
+
+
+
     
 
     
